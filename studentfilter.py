178d36377ca8879e6b6f963553672a2f51b99249
Samuel-Arter <samarter36@gmail.com>
2024-11-04 17:00:05 +1100

    Studentfilter: Added Docstrings

--- studentfilter.py
+++ studentfilter.py
@@ -1,5 +1,15 @@
 
 def select_students(students, threshold):
+    """
+    A function to determine which students have/haven't met a specific threshold
+
+    Argument(s):
+        1. (students) A list where each element is a list of a student name, and his grade.
+        2. (threshold) An integer which determines whether a student will be accepted/rejected
+
+    Returns:
+        result - A dictionary containing ordered sets of lists of 'Accepted' and 'Rejected' students and their grade
+    """
 
     output = {'Accepted': [], 'Rejected': []}
 
@@ -10,12 +20,16 @@ def select_students(students, threshold):
         else:
             output['Rejected'].append(student_score)
 
-    output1 = {'Accepted': sorted(output['Accepted'], key=lambda x: x[1], reverse=False), 'Rejected': sorted(output['Rejected'], key=lambda x: x[1], reverse=True)}
+    result = {'Accepted': sorted(output['Accepted'], key=lambda x: x[1], reverse=True), 'Rejected': sorted(output['Rejected'], key=lambda x: x[1], reverse=False)}
 
-    return output1
+    return result
 
 
 def select_student(students, threshold):
+    """
+    This function that returns a dictionary containing an ordered set of lists of 'accepted' and 'rejected'
+    students. This function is meant to be an improved version of the previous function.
+    """
     accepted = []
     rejected = []
 
@@ -34,4 +48,5 @@ def select_student(students, threshold):
 
 if __name__ == "__main__":
     my_class = [['Kermit Wade', 27], ['Hattie Schleusner', 22], ['Ben Ball', 54], ['William Lee', 2], ['Henry Lee', 15]]
+    print(select_students(my_class, 20))
     print(select_student(my_class, 20))
