bc7ad98b2f7108f9bf5d684fcbd64a96ce9ee1f6
Samuel-Arter <samarter36@gmail.com>
2024-11-19 18:12:09 +1100

    missing_card.py: added comments

--- missing_card.py
+++ missing_card.py
@@ -1,15 +1,16 @@
 import itertools
 import random
 
-color = {"S", "H", "D", "C"}
-value = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
-combination = itertools.product(color, value)
+color = ["S", "H", "D", "C"]
+value = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
+# create a deck of 52 cards using cartesian product
+combination = itertools.product(color, value)
 deck = [''.join(x) for x in combination]
 
 
 def get_cards():
-
+    """generate a string with 51 unique cards from a deck of 52"""
     selected_cards = []
     deck_temp = deck.copy()
 
@@ -25,7 +26,7 @@ def get_cards():
 
 
 def missing_card(cards):
-
+    """Determines the missing card in a deck of 51 cards"""
     for card in deck:
         if card not in cards:
             return card
@@ -33,7 +34,7 @@ def missing_card(cards):
 
 if __name__ == "__main__":
     selected_cards = get_cards()
-    print("---"*40)
+    print("---"*60)
     print(f"the selected cards are: {selected_cards}")
     print(f"the missing card is: {missing_card(selected_cards)}")
-    print("---"*40)
+    print("---"*60)
